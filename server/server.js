306e35d0558c0090fbc3b6c3f044842348eaaf55
asantesylvesterkwame <intelligentson17@gmail.com>
2023-03-21 03:07:17 +0000

    setup a dev script

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-const express = require('express')
+const express = require('express');
+
+// starting express
+const app = express();
+
+// function for get request for routes
+app.get(("/"), (req,res) => {
+    res.json({message: "Welcome to the app"})
+});
+
+// listen for requests
+app.listen(4000, () => {
+    console.log("listening on port 4000!!");
+})
